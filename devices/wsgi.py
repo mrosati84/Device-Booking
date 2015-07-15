ea7fbfaa55f5954c5c58761fc73b498cdb944b4d
Matteo Rosati <mrosati@h-art.com>
2015-07-15 11:13:10 +0200

    refactor application settings

--- devices/wsgi.py
+++ devices/wsgi.py
@@ -1,14 +1,7 @@
-"""
-WSGI config for devices project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devices.settings")
+
+env = os.environ.get('DJANGO_ENV') or 'local'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", '.'.join(["devices.settings", env]))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devices.settings")
+    env = os.environ.get('DJANGO_ENV') or 'local'
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", '.'.join(["devices.settings", env]))
 
     from django.core.management import execute_from_command_line
 
